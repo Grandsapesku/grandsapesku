dde36d36bf3bfb0b01f7ae195b437842152da831
Grandsapesku <yurijsapesky@seznam.cz>
2026-01-20 22:34:37 +0300

    Update GrandSapeskuWebsite.jsx

--- components/GrandSapeskuWebsite.jsx
+++ components/GrandSapeskuWebsite.jsx
@@ -1,7 +1,9 @@
 export default function GrandSapeskuWebsite() {
   return (
-    <div>
-      <h1>Component works</h1>
+    <div className="min-h-screen bg-gray-50 text-gray-900">
+      <h1 className="text-3xl font-bold text-center p-10">
+        GrandSapesku funguje
+      </h1>
     </div>
   );
 }
