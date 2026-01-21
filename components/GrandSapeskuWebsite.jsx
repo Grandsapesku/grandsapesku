7e5438cb42ed4744d09101531e040cdff7432c99
Grandsapesku <yurijsapesky@seznam.cz>
2026-01-21 17:14:42 +0300

    Update GrandSapeskuWebsite.jsx

--- components/GrandSapeskuWebsite.jsx
+++ components/GrandSapeskuWebsite.jsx
@@ -60,6 +60,7 @@ export default function GrandSapeskuWebsite() {
             <button onClick={() => scrollToSection("specialni")}>Speciální podmínky</button>
             <button onClick={() => scrollToSection("cenik")}>Orientační ceník</button>
             <button onClick={() => scrollToSection("sluzby")}>Naše služby</button>
+            <button onClick={() => scrollToSection("sluzby")}>DOPLŇKOVÉ SLUŽBY</button>
           </div>
 
           {/* MOBILE MENU BUTTON */}
@@ -79,7 +80,7 @@ export default function GrandSapeskuWebsite() {
             <button onClick={() => scrollToSection("faq")}>FAQ</button>
             <button onClick={() => scrollToSection("kontakt")}>Kontaktujte nás</button>
             <button onClick={() => scrollToSection("specialni")}>Speciální podmínky</button>
-            <button onClick={() => scrollToSection("cenik")}>DOPLŇKOVÉ SLUŽBYk</button>
+            <button onClick={() => scrollToSection("cenik")}>DOPLŇKOVÉ SLUŽBY</button>
           </div>
         )}
       </nav>
